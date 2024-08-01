a57a98983567eb4e1a69a8ffbe4fc98d67b32faf
Bernhard-Keller123 <bkeller1@gwdg.de>
2024-08-01 16:22:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ else:
     st.write(f" ")
 
 # URL of the trainingsdaten.json file in your GitHub repository
-url = "https://raw.githubusercontent.com/Bernhard-Keller123/AventraGPT_MK/main/trainingdata.json"
+url = "https://raw.githubusercontent.com/Bernhard-Keller123/AventraGPT/main/trainingdata.json"
 
 
 # Funktion zum Laden der Trainingsdaten von GitHub
